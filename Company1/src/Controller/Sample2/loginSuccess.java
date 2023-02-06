21b629036108b39aff373c8f0927cab7ddc147c1
kanada666 <tinachen2789@gmail.com>
2023-02-06 10:25:19 +0800

    20230206_1024
    test01

---
+++ Company1/src/Controller/Sample2/loginSuccess.java
@@ -0,0 +1,64 @@
+package Controller.Sample2;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class loginSuccess extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					loginSuccess frame = new loginSuccess();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public loginSuccess() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("登入成功");
+		lblNewLabel.setFont(new Font("微软雅黑", Font.BOLD, 20));
+		lblNewLabel.setBounds(173, 76, 85, 53);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("返回");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				loginUI l=new loginUI();
+				l.show();
+				dispose();
+			}
+		});
+		btnNewButton.setBounds(173, 161, 87, 23);
+		contentPane.add(btnNewButton);
+	}
+
+}
